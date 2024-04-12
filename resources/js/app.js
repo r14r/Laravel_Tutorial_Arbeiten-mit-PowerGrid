6cf4067c95ca5566ebd92b90a60a0c35b764acda
Ralph Göstenmeier <r14r@via-internet.de>
2024-04-12 17:11:54 +0200

    -

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+
+import './../../vendor/power-components/livewire-powergrid/dist/powergrid'
+
+// If you use Tailwind
+// import './../../vendor/power-components/livewire-powergrid/dist/tailwind.css'
+
+// If you use Bootstrap 5
+import './../../vendor/power-components/livewire-powergrid/dist/bootstrap5.css'
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,6 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard',  function () { return view('dashboard'); })->name('dashboard');
+    Route::get('/playground', function () { return view('playground'); })->name('playground');
 });
